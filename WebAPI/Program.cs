364ece984162f1df8445bba00588f94f72ac3487
matdon90 <mdonhefner@gmail.com>
2020-09-29 16:18:40 +0200

    Added initial configuration. Added MediatR. Added AutoMapper. Added Swagger. Added main api controller. Added default mapping profile and mapping interface.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,19 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
 namespace WebAPI
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
