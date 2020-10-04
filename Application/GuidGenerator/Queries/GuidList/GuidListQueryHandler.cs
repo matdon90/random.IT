381174884bd90702ddf8478d8ab99cfafa9b5f47
matdon90 <mdonhefner@gmail.com>
2020-10-04 21:45:01 +0200

    Added generation of many guid declared in request.

---
+++ Application/GuidGenerator/Queries/GuidList/GuidListQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.GuidGenerator.Queries.GuidList
+{
+    public class GuidListQueryHandler : IRequestHandler<GuidListQuery, List<Guid>>
+    {
+        private readonly IGuidGenerator _guidGenerator;
+        private readonly IDateTime _dateTime;
+
+        public GuidListQueryHandler(IGuidGenerator guidGenerator, IDateTime dateTime)
+        {
+            _guidGenerator = guidGenerator;
+            _dateTime = dateTime;
+        }
+        public Task<List<Guid>> Handle(GuidListQuery request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_guidGenerator.GuidGenerateMultiple(_dateTime.Now,request.GuidNumbers).ToList());
+        }
+    }
+}
