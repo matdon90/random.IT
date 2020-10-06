035d5ac633ceb5b833445fa474dc62d1d8c9032b
matdon90 <mdonhefner@gmail.com>
2020-10-06 23:27:55 +0200

    Adding version 4 of algorithm for GUID generation. Changing method in guid list generation.

--- Application/Common/Services/GuidGeneratorService.cs
+++ Application/Common/Services/GuidGeneratorService.cs
@@ -33,15 +33,15 @@ namespace Application.Common.Services
         }
 
         /// <summary>
-        /// Random node bytes generation
+        /// Random bytes generation
         /// </summary>
         /// <returns></returns>
-        private byte[] RandomNodeBytes()
+        private byte[] RandomBytes(int bytesNumber)
         {
-            var nodeBytes = new byte[6];
-            _random.NextBytes(nodeBytes);
+            var bytes = new byte[bytesNumber];
+            _random.NextBytes(bytes);
 
-            return nodeBytes;
+            return bytes;
         }
 
         /// <summary>
@@ -54,7 +54,7 @@ namespace Application.Common.Services
             byte[] guidByteArray = new byte[_guidByteArraySize];
             byte[] timestamp = BitConverter.GetBytes(date.Ticks - new DateTime(1582, 10, 15, 0, 0, 0).Ticks);
             byte[] clockSequence = BitConverter.GetBytes(Convert.ToInt16(Environment.TickCount % Int16.MaxValue));
-            byte[] nodeId = RandomNodeBytes();
+            byte[] nodeId = RandomBytes(6);
 
 			//insterting timestamp bytes into GUID
 			Array.Copy(timestamp, 0, guidByteArray, _timestampByteIndex, Math.Min(8, timestamp.Length));
@@ -76,17 +76,58 @@ namespace Application.Common.Services
 			return new Guid(guidByteArray);
 		}
 
+        /// <summary>
+        /// Guid generator using version 4 of Guid generation algorithm based on random numbers
+        /// </summary>
+        /// <returns></returns>
+        private Guid GenerateGuidV4()
+        {
+            byte[] guidByteArray = new byte[_guidByteArraySize];
+            byte[] firstPart = RandomBytes(8);
+            byte[] clockSequence = BitConverter.GetBytes(Convert.ToInt16(Environment.TickCount % Int16.MaxValue));
+            byte[] lastPart = RandomBytes(6);
+
+            //insterting timestamp bytes into GUID
+            Array.Copy(firstPart, 0, guidByteArray, 0, Math.Min(8, firstPart.Length));
+
+            //inserting clock sequence bytes into GUID
+            Array.Copy(clockSequence, 0, guidByteArray, 8, Math.Min(2, clockSequence.Length));
+
+            //inserting node ID bytes into GUID
+            Array.Copy(lastPart, 0, guidByteArray, 10, Math.Min(6, lastPart.Length));
+
+            //setting the version
+            guidByteArray[_versionByteIndex] &= (byte)_versionByteMask;
+            guidByteArray[_versionByteIndex] |= (byte)(_versionByteShift);
+
+            //setting the variant
+            guidByteArray[_variantByteIndex] &= (byte)_variantByteMask;
+            guidByteArray[_variantByteIndex] |= (byte)0x40;
+
+            return new Guid(guidByteArray);
+        }
+
+        /// <summary>
+        /// Method to generate single GUID with algorithm version 1 based on date.
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
         public Guid GuidGenerateSingle(DateTime date)
         {
             return GenerateGuidV1(date);
         }
 
-        public IEnumerable<Guid> GuidGenerateMultiple(DateTime date, int numberOfGuid)
+        /// <summary>
+        /// Method to generate multiple GUID with algorithm version 4 based on random data.
+        /// </summary>
+        /// <param name="numberOfGuid"></param>
+        /// <returns></returns>
+        public IEnumerable<Guid> GuidGenerateMultiple(int numberOfGuid)
         {
             List<Guid> guids = new List<Guid>();
             for (int i = 0; i < numberOfGuid; i++)
             {
-                guids.Add(GenerateGuidV1(date));
+                guids.Add(GenerateGuidV4());
             };
             return guids;
         }
