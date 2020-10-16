dc39a6e75545aa717a16ddf5f413921ab7a548f2
matdon90 <mdonhefner@gmail.com>
2020-10-16 16:38:07 +0200

    Test for Single Guid and Guid List implemented.

---
+++ Application.IntegrationTests/GuidGenerator/Queries/GuidListTests.cs
@@ -0,0 +1,60 @@
+﻿using Application.GuidGenerator.Queries.GuidList;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.IntegrationTests.GuidGenerator.Queries
+{
+    using static Testing;
+
+    public class GuidListTests : TestBase
+    {
+        [Test]
+        public async Task Should_ReturnNotEmptyOrNullList()
+        {
+            //arrange
+            var query = new GuidListQuery(10);
+
+            //act
+            var result = await SendAsync(query);
+
+            //assert
+            result.Should().NotBeNullOrEmpty();
+        }
+
+        [Test]
+        public async Task Should_ReturnListOfTenGuids()
+        {
+            //arrange
+            var query = new GuidListQuery(10);
+
+            //act
+            var result = await SendAsync(query);
+
+            //assert
+            result.Should().HaveCount(10);
+            for (int i = 0; i < 10; i++)
+            {
+                result[i].Should<Guid>().BeOfType<Guid>();
+            }
+        }
+
+        [Test]
+        public async Task Should_ReturnListWithNoRepeats()
+        {
+            //arrange
+            int guidNumber = 100000;
+            var query = new GuidListQuery(guidNumber);
+
+
+            //act
+            var result = await SendAsync(query);
+
+            //assert
+            result.Should().HaveCount(guidNumber);
+            result.Should().OnlyHaveUniqueItems();
+        }
+    }
+}
---
+++ Application.IntegrationTests/GuidGenerator/Queries/GuidSingleTests.cs
@@ -0,0 +1,59 @@
+﻿using Application.GuidGenerator.Queries.GuidSingle;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.IntegrationTests.GuidGenerator.Queries
+{
+    using static Testing;
+
+    public class GuidSingleTests : TestBase
+    {
+        [Test]
+        public async Task Should_ReturnNotNull()
+        {
+            //arrange
+            var query = new GuidSingleQuery();
+
+            //act
+            var result = await SendAsync(query);
+
+            //assert
+            result.Should().NotBeEmpty();
+        }
+
+        [Test]
+        public async Task Should_ReturnGuidType()
+        {
+            //arrange
+            var query = new GuidSingleQuery();
+
+            //act
+            var result = await SendAsync(query);
+
+            //assert
+            result.Should<Guid>().BeOfType<Guid>();
+        }
+
+        [Test]
+        public async Task Should_ReturnNoRepeats()
+        {
+            //arrange
+            var guidList = new List<Guid>();
+            var query = new GuidSingleQuery();
+            int guidNumber = 100000;
+
+            //act
+            for (int i = 0; i < guidNumber; i++)
+            {
+                guidList.Add(await SendAsync(query));
+            }
+
+            //assert
+            guidList.Should().HaveCount(guidNumber);
+            guidList.Should().OnlyHaveUniqueItems();
+        }
+    }
+}
